0e27d20793359d937c078b0f6324f42f00cde6bf
gabor_czigola <gabor_czigola@epam.com>
2016-08-14 19:51:57 +0800

    initial pager

---
+++ README.md
@@ -0,0 +1,40 @@
+## Pagination support for the GitHub API
+
+
+Given you are using the GitHub API, this package can help you to retrieve ALL results instead of just the first perPage. GitHub API is of this writing limited to 100 results. In case you need all results, this module will retrieve all matches for your request, by parsing the links response attribute and following up to the next page until there is one, then returning all results together like in a sinlg call. 
+
+This release is compatible with the following setup:
+https://github.com/mikedeboer/node-github
+https://developer.github.com/v3/
+
+It is written in plain old JavaScript for your convenience.
+
+### Usage example
+
+To retrieve all pull requests between to dates:
+
+```javascript
+githubPager.readAllUnique(github.search.issues, {
+    q: 'repo:${options.owner}/${options.repory type:pr created:${options.dateFrom}..${options.dateTo}',
+    sort: 'created',
+    order: 'asc'
+})
+.then(...)
+.catch(...);
+```
+
+### API specification
+
+##### readAll(method, options)
+
+Execute the GitHub API method with given options and return its promise. perPage defaults to 100.
+Duplicate items are possible in the resolved response.
+
+##### readAllUnique(method, options) 
+
+Execute the GitHub API method with given options and return its promise. perPage defaults to 100.
+Duplicate items are filtered using the number attribute as unique key.
+
+##### parseLinks(links)
+
+Synchronous method to parse GitHub API links attribute. Returns array of links.
---
+++ index.js
@@ -0,0 +1,55 @@
+var Q = require("Q");
+var _ = require("lodash");
+
+var MAX_PER_PAGE = 100;
+
+function parseLinks(links) {
+    return links.split(",").reduce(function(result, value){
+        var rel = /<([^>]+)>; rel="([^"]+)"/.exec(value);
+        result[rel[2]] = rel[1];
+        return result;
+    }, {});
+}
+
+function readAll(method, options) {
+    var future = Q.defer();
+
+    (function readRecursive(page, resultsSoFar) {
+        var pageOptions = _.extend(options, {
+            page: page,
+            perPage: options.perPage ? options.perPage : MAX_PER_PAGE;
+        });
+        Q.nfcall(method, pageOptions).done(function(response){
+            if (response.incomplete_results !== false) {
+                future.reject(response);
+            }
+            var results = resultsSoFar.concat(response.items);
+            var hasNextPage = response.meta.link && parseLinks(response.meta.link)["next"];
+            if (hasNextPage) {
+                readRecursive(page + 1, results);
+            } else {
+                future.resolve(results);
+            }
+        });
+    })(1, []);
+
+    return future.promise;
+}
+
+function reduceUnique(items) {
+    var uniqueItemsMap = items.reduce(function(result, value){
+        result[value.number] = value;
+        return result;
+    }, {});
+    return _.values(uniqueItemsMap);
+}
+
+function readAllUnique(method, options) {
+    return reduceUnique(readAll(method, options));
+}
+
+module.exports = {
+    parseLinks: parseLinks,
+    readAll: readAll,
+    reduceUnique: reduceUnique
+}
